746edbd6d69d226e072c0ed159f8ae51ec48a0d1
Yanis <yanis@netkoders.com>
2021-09-09 17:00:18 +0200

    LA CASTAGNE

--- Personnage.php
+++ Personnage.php
@@ -7,16 +7,47 @@ class Personnage
     private $_experience = 1;
     private $_degats = 0;
 
-    public function definirForce($force){
+    public function __construct($nom, $force = 50, $degats = 0){
+        $this->setNom($nom);
+        $this->setForce($force);
+        $this->setDegats($degats);
+        $this->setExperience(1);
+    }
+
+    public function __toString(){
+        return $this->getNom() . "(". $this->getDegats() .")";
+    }
+
+    public function setNom($nom){
+        if (!is_string($nom)){
+            trigger_error('Le nom d\'un personnage doit être un texte', E_USER_ERROR);
+            return;
+        }
+        $this->_nom = $nom;
+    }
+
+    
+    public function getNom(){
+        return $this->_nom;
+    }
+
+    public function setForce($force){
+        if (!is_int($force)){
+            trigger_error('La force d\'un personnage doit être un nombre entier', E_USER_ERROR);
+            return;
+        }
+        if ($force > 100){
+            trigger_error('La force d\'un personnage ne doit pas dépasser 100', E_USER_ERROR);
+            return;
+        }
         $this->_force = $force;
     }
 
-    public function frapper($adversaire){
-        // $adversaire->_degats += $this->_force; idem a :
-            $adversaire->_degats = $adversaire->_degats + $this->_force;
+    public function getforce(){
+        
     }
 
-    public function definirExperience($_experience){
+    public function setExperience($_experience){
         $this->_experience = $_experience;
     }
 
@@ -28,12 +59,26 @@ class Personnage
         return $this->_experience;
     }
 
-    public function afficheDegats(){
+    public function setDegats($degats){
+        if (!is_int($degats)){
+            trigger_error('Les dégats d\'un personnage doit être un nombre entier', E_USER_ERROR);
+            return;
+        }
+        $this->_degats = $degats;
+    }
+
+    public function getDegats(){
         return $this->_degats;
     }
 
     public function parler(){
-        print("Hé TOI JVAI MANGER T MORT !");
+        print("Omae wa mō shindeiru.");
     }
 
+    public function frapper(Personnage $adversaire){
+        // $adversaire->_degats += $this->_force; idem a :
+            $adversaire->_degats = $adversaire->_degats + $this->_force;
+            $this->gagnerExperience();
+            print('<div>'. $adversaire .'c\'est pris une mandale par ' . $this .' ==> Dégats de '. $adversaire . ' = '. $adversaire .'</div>');
+    }
 }
--- index.php
+++ index.php
@@ -1,26 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>LA CASTAGNE</title>
+</head>
+<body>
+<h1>JEU DE LA CASTAGNE</h1>
+</body>
+</html>
 <?php
 
 include "Personnage.php";
-
-print("<h1>JEU DE CASTAGNE</h1>");
 
 // $perso = new Personnage();
 // $perso->parler();
 // $perso->gagnerExperience();
 // print("<div>XP = " . $perso->afficheExperience() . "</div>");
 
-$perso1 = new Personnage();
-$perso1->definirForce(20);
-$perso1->definirExperience(12);
+$perso1 = new Personnage("ken", 100, 0);
+// $perso1->definirForce(200000000000);
+$perso1->setExperience(999999999999);
 
-$perso2 = new Personnage();
-$perso2->definirForce(21);
-$perso2->definirExperience(15);
+$perso2 = new Personnage("vilain méchant pas gentil");
+// $perso2->definirForce(21);
+$perso2->setExperience(15);
 
-print("Perso1 = ");
+print("ken = ");
 print($perso1->parler());
 $perso1->frapper($perso2);
 $perso2->frapper($perso1);
 
-print("<div>XP p1 = " . $perso1->afficheExperience() . " degats = " . $perso1->afficheDegats() ."</div>");
-print("<div>XP p2 = " . $perso2->afficheExperience() . " degats = " . $perso2->afficheDegats() ."</div>");
+print("<div> XP de ken = " . $perso1->afficheExperience() ."</div>");
+print("<div>XP du vilain méchant pas gentil = " . $perso2->afficheExperience() ."</div>");
